2e408cfe368f68fcc0c809297cf17b71d5c2ea14
Ilan Bar-Magen <ilanbm@ilanmac.home>
2019-04-12 03:35:11 +0300

    Dah!

---
+++ background.js
@@ -0,0 +1,11 @@
+
+chrome.tabs.onActivated.addListener( activeInfo => {
+		chrome.windows.getAll({populate: true}, windowList => 
+					windowList.forEach(window => 
+									window.tabs.forEach( tab => 
+													tab.audible && !tab.pinned && chrome.tabs.update(tab.id, {muted: true}) )
+					)
+		);
+	chrome.tabs.getSelected(null, tab => chrome.tabs.update(tab.id, {muted: false}) );
+})
+

1431f84481a9026ad12d4f965bbdea2cbabf3a51
Ilan Bar-Magen <ilanbarmagen@gmail.com>
2019-08-14 00:12:45 +0300

    removed comments

--- background.js
+++ background.js
@@ -1,11 +1,5 @@
 
 chrome.tabs.onActivated.addListener( activeInfo => {
-	// chrome.windows.getAll({populate: true}, windowList => 
-	// 	windowList.forEach(window => 
-	// 		window.tabs.forEach( tab => 
-	// 			tab.audible && !tab.pinned && chrome.tabs.update(tab.id, {muted: true}) )
-	// 	)
-	// );
 	chrome.windows.getCurrent({populate: true}, window => window.tabs.forEach( tab => 
 				tab.audible && !tab.pinned && chrome.tabs.update(tab.id, {muted: true}) ))
 	chrome.tabs.getSelected(null, tab => chrome.tabs.update(tab.id, {muted: false}) );
